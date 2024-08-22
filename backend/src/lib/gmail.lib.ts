435523d8aba20b3a97700b6513c020b3c4e0e898
shashwat <shashwat1461.cse18@chitkara.edu.in>
2024-08-23 02:10:00 +0530

    transactions: rethinking strategy

--- backend/src/lib/gmail.lib.ts
+++ backend/src/lib/gmail.lib.ts
@@ -4,6 +4,7 @@ import RequestManager from "./request-manager.lib";
 class GmailClient{
     private static _instance: GmailClient;
     private requestManager: RequestManager;
+    private  _limit = 50;
     private constructor(){
         this.requestManager = new RequestManager(process?.env?.GMAIL_API_ENDPOINT_URL!)
     }
@@ -16,12 +17,11 @@ class GmailClient{
     }
     async getEmails(accessToken: string, query: string){
         console.log('getting emails')
-        const response = await this.requestManager.client.get<GmailMessages>(`/me/messages?${query}`, {
+        const response = await this.requestManager.client.get<GmailMessages>(`/me/messages?maxResults=${this._limit}&${query}`, {
             headers: {
                 Authorization: `Bearer ${accessToken}`
             }
         })
-
         return response
     }
 }
